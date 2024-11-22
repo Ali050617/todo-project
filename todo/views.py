66a4758fccc7acc067b4667167e15502ee486bea
Muhammadali <muhammadalikadyrov06@gmail.com>
2024-11-22 18:59:17 +0500

    kament

--- todo/views.py
+++ todo/views.py
@@ -2,17 +2,41 @@ from django.shortcuts import HttpResponse
 
 def task_create(request):
     html_response = """
+      <!DOCTYPE html>
+<html lang="uz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Vazifa</title>
+</head>
+<body>
     <h1>Yangi vazifa yaratish</h1>
-    
-    <br>
-    
     <form>
-        <label for="Vazifa nomi>
-        Vazifa nomi:
-        <input type="text"/>
-        </label>
-    
+        <label>Vazifa nomi:</label>
+        <input type="text" required>
+        <br><br>
+        
+        <label>Tavsif:</label>
+        <textarea rows="4" cols="50"></textarea>
+        <br><br>
+        
+        <label>Muddat:</label>
+        <input type="date">
+        <br><br>
+        
+        <label>Muhimlik darajasi:</label>
+        <select>
+            <option>Past</option>
+            <option>O‘rta</option>
+            <option>Yuqori</option>
+        </select>
+        <br><br>
+        
+        <button>Saqlash</button>
     </form>
+</body>
+</html>
+    
     
     """
-    return HttpResponse(task_create())
+    return HttpResponse(task_create)
